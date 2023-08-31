f0af5a088e9e0857141e964331e90c268b1ad2fe
Lucas Ferreira <lucascandido.ferreira@hotmail.com>
2023-08-31 19:19:04 -0300

    criativa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { ServicosComponent } from './servicos/servicos.component';
+import { PortifolioComponent } from './portifolio/portifolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    ServicosComponent,
+    PortifolioComponent
   ],
   imports: [
     BrowserModule
